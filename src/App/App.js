3b4d0ad23d439c3ee3be0bc83f3758348a1382a7
Adam Wieckert <wiec1369@gmail.com>
2018-07-12 12:46:45 -0500

    Working on setting up all the routes

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
+import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
+import AllTheStuff from '../components/AllTheStuff/AllTheStuff.js';
+import Home from '../components/Home/Home.js';
+import Login from '../components/Login/Login.js';
+import MyStuff from '../components/MyStuff/MyStuff.js';
+import Register from '../components/Register/Register.js';
+import Navbar from '../components/Navbar/Navbar.js';
 import './App.css';
 
 class App extends Component {
-  render() {
+  state = {
+    authed: false,
+  };
+
+  logOut = () => {
+    this.setState({authed: false});
+  };
+
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <BrowserRouter>
+          <div>
+            <Navbar authed={this.state.authed} logOut={this.logOut} />
+            <div className='container'>
+              <div className='row'>
+                <Switch>
+                  <Route path='/' exact component={Home}/>
+                  <PublicRoute path='/register' authed={this.state.authed} component={Register}/>
+                  <PublicRoute path='/login' authed={this.state.authed} component={Login}/>
+                  <PrivateRoute path='/MyStuff' authed={this.state.authed} component={MyStuff}/>
+                  <PrivateRoute path='/order/:id' authed={this.state.authed} component={AllTheStuff}/>
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/AllTheStuff/AllTheStuff.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './AllTheStuff.css';
+
+class AllTheStuff extends React.Component {
+  render () {
+    return (
+      <div className="AllTheStuff">
+        <h1>All The Stuff</h1>
+      </div>
+    );
+  }
+}
+
+export default AllTheStuff;
